e91c4588eb727c2e9a8ffce8c839512eeff6170f
manuxpr <manuelnb@iescastelar.com>
2024-05-30 09:41:51 +0200

    Eliminados varios niveles de la interface pokemonData

--- git-test/src/components/Home.tsx
+++ git-test/src/components/Home.tsx
@@ -1,93 +1,140 @@
-import { useState } from "react";
-import {Button, Card, CardContent, CardMedia, Typography, CardHeader, Avatar, Box, IconButton } from '@mui/material';
-import { blue } from '@mui/material/colors';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useState } from 'react';
+import { Button, Card, CardContent, CardMedia, Typography, CardHeader, Box } from '@mui/material';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // Importa los estilos de React Quill
 
 interface PokemonData {
-    name: string;
-    abilities: { ability: { name: string } }[];
-    sprites: { front_default: string };
-  }
+  name: string;
+  abilities: Ability[];
+  sprites: Sprites;
+}
+
+export interface Ability {
+  ability:   Species;
+  is_hidden: boolean;
+  slot:      number;
+}
+
+export interface Species {
+  name: string;
+  url:  string;
+}
+
+export interface Sprites {
+  back_default:       string;
+  back_female:        null;
+  back_shiny:         string;
+  back_shiny_female:  null;
+  front_default:      string;
+  front_female:       null;
+  front_shiny:        string;
+  front_shiny_female: null;
+  animated?:          Sprites;
+}
 
 const Home = () => {
-    const [data, setData] = useState<PokemonData | null>(null); 
-    const fetchData = async () => {
-        const endpointUrl = import.meta.env.VITE_ENDPOINT_URL;
-        const response = await fetch(endpointUrl);
-        const data: PokemonData = await response.json();
-        setData(data);
-    }
+  const [data, setData] = useState<PokemonData | null>(null);
+  const [editorContent, setEditorContent] = useState('');
 
-    return (
-        <Box
+  const fetchData = async () => {
+    const endpointUrl = import.meta.env.VITE_ENDPOINT_URL;
+    const response = await fetch(endpointUrl);
+    const data: PokemonData = await response.json();
+    setData(data);
+  };
+
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Button
+        variant="outlined"
+        color="secondary"
+        onClick={fetchData}
         sx={{
+          marginTop: '20px',
+          marginBottom: '20px',
+          padding: '10px 20px',
+          borderRadius: '12px',
+          borderWidth: '2px',
+          '&:hover': {
+            borderColor: 'darkpurple',
+            color: 'darkpurple',
+          },
+        }}
+      >
+        Traer datos de la API
+      </Button>
+            {data && (
+        <Card
+          sx={{
+            width: '300px',
+            mt: 2,
+            boxShadow: 3,
+            borderRadius: '12px',
+            borderColor: 'darkpurple',
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            justifyContent: 'center',
-        }}>
-            <Button variant="outlined" color="secondary" onClick={fetchData} 
-            sx={{
-                    marginTop: '20px',
-                    marginBottom: '20px',
-                    padding: '10px 20px',
-                    borderRadius: '12px',
-                    borderWidth: '2px',
-                    '&:hover': {
-                        borderColor: 'darkpurple',
-                        color: 'darkpurple',
-                    }
-                }}>
-                Traer datos de la API
-            </Button>
-            {data && (
-                <Card 
-                sx={{
-                    width: '300px',
-                    mt: 2, 
-                    boxShadow: 3,
-                    borderRadius: '12px',
-                    borderColor: 'darkpurple', 
-                    alignItems: 'center',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    
+            backgroundColor: '#ffffff',
+            padding: '20px',
+          }}
+        >
+          <CardHeader
+            avatar={data.name.charAt(0).toUpperCase()
+              
+             } 
+            title={
+              <Typography variant="h6" color="text.primary">
+                {data.name.toUpperCase()}
+              </Typography>
+            }
+            subheader="Pokemon Abilities"
+            sx={{ textAlign: 'center' }}
+          />
+          <CardMedia
+            component="img"
+            height="194"
+            image={data.sprites.front_default}
+            alt={data.name}
+            sx={{ objectFit: 'contain', marginTop: '10px' }}
+          />
+          <CardContent sx={{ width: '100%', textAlign: 'center' }}>
+            <Typography variant="h6" color="text.primary">
+              Abilities:
+            </Typography>
+            {data.abilities.map((ability, index) => (
+              <Typography variant="body2" color="text.secondary" key={index}>
+                {ability.ability.name}
+              </Typography>
+            ))}
+          </CardContent>
+          <Box sx={{ width: '100%', mt: 2 }}>
+            <ReactQuill
+              theme="snow"
+              value={editorContent}
+              onChange={setEditorContent}
+              placeholder="Prueba de texto"
+              style={{ backgroundColor: 'white', borderRadius: '8px' }}
+            />
+          </Box>
+        </Card>
+      )}
 
-                }}>
-                    <CardHeader
-                        avatar={
-                        <Avatar sx={{ bgcolor: blue[500] }} aria-label="pokemon">
-                            {data.name.charAt(0).toUpperCase()}
-                        </Avatar>
-                        }
-                        action={
-                        <IconButton aria-label="settings">
-                            <MoreVertIcon />
-                        </IconButton>
-                        }
-                        title={data.name.toUpperCase()}
-                        subheader="Pokemon Abilities"
-                    />
-                    <CardMedia
-                        component="img"
-                        height="194"
-                        image={data.sprites.front_default}
-                        alt={data.name}
-                    />
-                    <CardContent>
-                        <Typography variant="h6" color="text.primary">
-                        Abilities:
-                        </Typography>
-                        {data.abilities.map((ability, index) => (
-                        <Typography variant="body2" color="text.secondary" key={index}>
-                            {ability.ability.name}
-                        </Typography>
-                        ))}
-                    </CardContent>
-                </Card>
-            )}
-        </Box>
-    )
-}
+    </Box>
+  );
+};
+
+
 
 export default Home;
+
+
+
+
--- git-test/src/components/Login.tsx
+++ git-test/src/components/Login.tsx
@@ -4,7 +4,7 @@ import { TextField, Button } from '@mui/material';
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [savedData, setSavedData] = useState<{email: string, password: string} | null>(null);
+  const [savedData, setSavedData] = useState<{ email: string, password: string } | null>(null);
 
   const handleSubmit = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
@@ -18,50 +18,50 @@ function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit} style={{ 
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-  }}>
-      <TextField 
-        label="Email" 
-        variant="outlined" 
-        type="email" 
-        required 
-        value={email} 
-        onChange={(e) => setEmail(e.target.value)} 
-        sx={{ 
+    <form onSubmit={handleSubmit} style={{
+      alignItems: 'center',
+      justifyContent: 'center',
+      display: 'flex',
+    }}>
+      <TextField
+        label="Email"
+        variant="outlined"
+        type="email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        sx={{
           margin: 2,
           display: 'flex',
           marginTop: 2,
           borderColor: 'darkred',
-         }}
+        }}
       />
-      <TextField 
-        label="Password" 
-        variant="outlined" 
-        type="password" 
-        required 
-        value={password} 
-        onChange={(e) => setPassword(e.target.value)} 
+      <TextField
+        label="Password"
+        variant="outlined"
+        type="password"
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
       <Button variant="outlined" color="error" type="submit"
-      sx={{
-        display: 'flex',
-        marginTop: 2,
-        marginBottom: 2,
-        marginLeft: 2,
-      }} >
+        sx={{
+          display: 'flex',
+          marginTop: 2,
+          marginBottom: 2,
+          marginLeft: 2,
+        }} >
         Login
       </Button>
       <Button variant="outlined" color="primary" onClick={handleShowData}
-      sx={{
-        display: 'flex',
-        marginTop: 2,
-        marginBottom: 2,
-        marginLeft: 2,
-      }} >
-        Show Saved Data
+        sx={{
+          display: 'flex',
+          marginTop: 2,
+          marginBottom: 2,
+          marginLeft: 2,
+        }} >
+        datos guardados
       </Button>
     </form>
   );
